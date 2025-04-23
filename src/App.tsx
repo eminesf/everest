fb01a799b94b402e444b94629fe12e1847e7df8c
Emiliano <emilianofuchs@gmail.com>
2025-04-22 21:27:16 -0300

    chore: update in the path logo

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: FC = () => {
     <section className="container">
       <div className="container_content">
         <div className="container_content__logo">
-          <img src="../public/everest-logo.svg" alt="" />
+          <img src="/everest-logo.svg" alt="" />
         </div>
         <InputField
           size="md"
